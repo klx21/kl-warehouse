4b0403ce7faad33a47708520ffb3b43e340bf73c
Huan Li <klx211@gmail.com>
2020-10-16 23:15:25 -0500

    * Added method `deserializeBreadthFirst()`, `isSymmetric()` and `serializeWithHashes()`.

--- exercises/data-structure/binary-search-tree.mjs
+++ exercises/data-structure/binary-search-tree.mjs
@@ -22,16 +22,71 @@ export class BinarySearchTree {
     this.root = root || null;
   }
 
+  /**
+   * Create a binary search tree from the given array.
+   *
+   * @param {Array<number|string>} array An breadth first array of tree node values. Empty node is represented by '#'.
+   * @return {BinarySearchTree} A binary search tree.
+   */
+  deserializeBreadthFirst(array) {
+    if (!this.root && array.length > 0 && array[0] !== '#') {
+      if (array.length === 1) {
+
+        this.root = new TreeNode(array.pop());
+      } else {
+        const nodes = array.map(element => element === '#' ? '#' : new TreeNode(parseInt(element, 10)));
+        const tree = [[]];
+        let level = 0;
+
+        while (nodes.length > 0) {
+          const currentNode = nodes.shift();
+          const levelIndex = tree[level].length;
+
+          if (level > 0 && currentNode !== '#') {
+            const parentLevel = level - 1;
+            const parentIndex = Math.floor(levelIndex / 2);
+            const parentNode = tree[parentLevel][parentIndex];
+
+            if (parentNode === '#') {
+              throw 'Parent node is empty. The array may be wrong.';
+            }
+
+            const childKey = levelIndex % 2 === 0 ? 'left' : 'right';
+            parentNode[childKey] = currentNode;
+          }
+
+          tree[level].push(currentNode);
+
+          if (levelIndex === Math.pow(2, level) - 1) {
+            tree[++level] = [];
+          }
+        }
+
+        this.root = tree[0][0];
+      }
+    }
+
+    return this;
+  }
+
   insertInOrder(value, allowDuplicate) {
     const newNode = new TreeNode(value);
 
     if (!this.root) {
       this.root = newNode;
     } else {
-      this.addSide(this.root, newNode, allowDuplicate);
+      this.addSideInOrder(this.root, newNode, allowDuplicate);
     }
 
     return this;
+  }
+
+  isSymmetric() {
+    if (!this.root) {
+      return true;
+    } else {
+      return this.checkNodesSymmetry(this.root.left, this.root.right);
+    }
   }
 
   searchBreadthFirst(target) {
@@ -59,7 +114,35 @@ export class BinarySearchTree {
     return this.traverseFor(target, this.root, depthFirstOrder || DEPTH_FIRST_ORDER.IN_ORDER) || null;
   }
 
+  serializeWithHashes() {
+    const visited = [];
+    const queue = [this.root];
+    let level = 0;
 
+    do {
+      const current = queue.shift();
+      visited.push(current === '#' ? '#' : current.value);
+      if (current.left) {
+        queue.push(current.left);
+      } else {
+        queue.push('#');
+      }
+      if (current.right) {
+        queue.push(current.right);
+      } else {
+        queue.push('#');
+      }
+      if (visited.length === this.getGeometricSum(level)) {
+        if (this.allHashes(queue)) {
+          return visited
+            .join(';')
+            .replace(/(;#)+$/, '')
+            .split(';');
+        }
+        level++;
+      }
+    } while (queue.length > 0);
+  }
 
   serializeToArray(treeDirection, depthFirstOrder) {
     switch (treeDirection) {
@@ -79,18 +162,53 @@ export class BinarySearchTree {
    * @param allowDuplicate
    * @returns {BinarySearchTree}
    */
-  addSide(currentNode, newNode, allowDuplicate) {
+  addSideInOrder(currentNode, newNode, allowDuplicate) {
     if (allowDuplicate || currentNode.value !== newNode.value) {
       const side = newNode.value <= currentNode.value ? 'left' : 'right';
       if (!currentNode[side]) {
         currentNode[side] = newNode;
         return this;
       } else {
-        return this.addSide(currentNode[side], newNode);
+        return this.addSideInOrder(currentNode[side], newNode);
       }
     } else {
       return this;
     }
+  }
+
+  allHashes(visited) {
+    if (visited.length === 0) {
+      return false;
+    } else {
+      return visited
+        .join('')
+        .replace(/#/g, '')
+        .length === 0;
+    }
+  }
+
+  checkNodesSymmetry(node1, node2) {
+    if (node1 === null && node2 === null) {
+      return true;
+    }
+    if (node1 === null || node2 === null) {
+      return false;
+    }
+    if (node1.val !== node2.val) {
+      return false;
+    }
+
+    return check(node1.left, node2.right) && check(node1.right, node2.left);
+  }
+
+  getGeometricSum(level) {
+    return Math.pow(2, level + 1) - 1;
+  }
+
+  isArraySymmetric(array) {
+    const firstHalf = array.slice(0, array.length / 2);
+    const secondHalf = array.slice(array.length / 2);
+    return firstHalf.join('') === secondHalf.reverse().join('');
   }
 
   /**
@@ -162,13 +280,25 @@ export class BinarySearchTree {
 
     switch (depthFirstOrder) {
       case DEPTH_FIRST_ORDER.PRE_ORDER:
-        results = [currentResult, leftResult, rightResult];
+        results = [
+          currentResult,
+          leftResult,
+          rightResult
+        ];
         break;
       case DEPTH_FIRST_ORDER.IN_ORDER:
-        results = [leftResult, currentResult, rightResult];
+        results = [
+          leftResult,
+          currentResult,
+          rightResult
+        ];
         break;
       case DEPTH_FIRST_ORDER.POST_ORDER:
-        results = [leftResult, rightResult, currentResult];
+        results = [
+          leftResult,
+          rightResult,
+          currentResult
+        ];
         break;
     }
 
--- exercises/data-structure/test.mjs
+++ exercises/data-structure/test.mjs
@@ -2,24 +2,31 @@ import { DEPTH_FIRST_ORDER, BinarySearchTree, TREE_DIRECTION } from './binary-se
 import { strict as assert } from 'assert';
 
 const tree = new BinarySearchTree();
-const numbers = [20, 14, 9, 19, 57, 31, 3, 62, 11, 72];
-numbers.forEach(number => tree.insertInOrder(number));
+// const numbers = [20, 14, 9, 19, 57, 31, 3, 62, 11, 72];
+const elements = [1,2,2,'#',3,'#',3];
+const elements1 = [1,2,2,3,4,4,3];
+const elements2 = [1,2,3];
+// numbers.forEach(number => tree.insertInOrder(number));
+
 
 try {
-  assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.BREADTH), [20, 14, 57, 9, 19, 31, 62, 3, 11, 72]);
-  console.info('Success: Breadth first serialization succeeded.');
-  assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.PRE_ORDER), [20, 14, 9, 3, 11, 19, 57, 31, 62, 72]);
-  console.info('Success: Pre-order depth first serialization succeeded.');
-  assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.POST_ORDER), [3, 11, 9, 19, 14, 31, 72, 62, 57, 20]);
-  console.info('Success: Post-order depth first serialization succeeded.');
-  assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.IN_ORDER), [3, 9, 11, 14, 19, 20, 31, 57, 62, 72]);
-  console.info('Success: In-order depth first serialization succeeded.');
-  assert.equal(tree.searchBreadthFirst(5), null);
-  console.info('Success: 5 is not found with a breadth first search.');
-  assert.ok(tree.searchDepthFirst(31), '31 is not found with an in-order depth first search.');
-  console.info('Success: 31 is found with an in-order depth first search.');
-  assert.equal(tree.searchDepthFirst(46), null);
-  console.info('Success: 46 is not found with an in-order depth first search.');
+  // assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.BREADTH), [20, 14, 57, 9, 19, 31, 62, 3, 11, 72]);
+  // console.info('Success: Breadth first serialization succeeded.');
+  // assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.PRE_ORDER), [20, 14, 9, 3, 11, 19, 57, 31, 62, 72]);
+  // console.info('Success: Pre-order depth first serialization succeeded.');
+  // assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.POST_ORDER), [3, 11, 9, 19, 14, 31, 72, 62, 57, 20]);
+  // console.info('Success: Post-order depth first serialization succeeded.');
+  // assert.deepStrictEqual(tree.serializeToArray(TREE_DIRECTION.DEPTH, DEPTH_FIRST_ORDER.IN_ORDER), [3, 9, 11, 14, 19, 20, 31, 57, 62, 72]);
+  // console.info('Success: In-order depth first serialization succeeded.');
+  // assert.equal(tree.searchBreadthFirst(5), null);
+  // console.info('Success: 5 is not found with a breadth first search.');
+  // assert.ok(tree.searchDepthFirst(31), '31 is not found with an in-order depth first search.');
+  // console.info('Success: 31 is found with an in-order depth first search.');
+  // assert.equal(tree.searchDepthFirst(46), null);
+  // console.info('Success: 46 is not found with an in-order depth first search.');
+  tree.deserializeBreadthFirst(elements2);
+  console.log(tree.serializeWithHashes());
+  console.log(tree.isSymmetric());
 } catch(e) {
   console.error('Failure: ', e.message);
 } finally {
